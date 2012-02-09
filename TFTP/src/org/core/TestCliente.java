5ce711874b27725b284461e47c42b78d3806d621
crmencs@gmail.com <crmencs@gmail.com@00cfcc58-b0cb-0e36-504f-72c9a4a937af>
2012-02-09 21:22:44 +0000

    Avance en el main del cliente

--- TFTP/src/org/core/TestCliente.java
+++ TFTP/src/org/core/TestCliente.java
@@ -1,15 +1,51 @@
 package org.core;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class TestCliente extends TFTP{
 
-	public static void main(String[] args)
+	public void main(String[] args) throws FileNotFoundException
 	{
+		int opcion=0;
+		byte[] nombreArchivo;
+		String hostServer="";	//<-------?
 		
+		String cadenaLeida;
+        System.out.println("Ejecución de cliente:\n");
+        System.out.println("Escriba \"salir\" para terminar la aplicacion.\n");
+		System.out.print("tftp> ");
+		Scanner sc = new Scanner(System.in);
+		cadenaLeida = sc.nextLine();
+		while(cadenaLeida.compareTo("Salir")!=0)
+		{
+			if(cadenaLeida.compareTo("get")==0)
+				opcion = 1;
+			if(cadenaLeida.compareTo("put")==0)
+				opcion = 2;
+		}
+		if(opcion!=0)
+		{
+			System.out.println("Introduzca el nombre del archivo");
+			cadenaLeida = sc.nextLine();
+			nombreArchivo = cadenaLeida.getBytes();
+			if(opcion==1)
+				get(hostServer,nombreArchivo.toString());
+			if(opcion==2)
+			{
+				File archivo = new File(nombreArchivo.toString());	//Crea uno nuevo sobreescribiendo el anterior?
+				put(hostServer,archivo);
+			}
+		}
+		else
+		{
+			System.out.println("Comando no válido. Escriba put para enviar un archivo o get para recibirlo\n");
+		}
+			
 	}
 	
-	private void get(String hostServer, String nombreArchivo)
+	public void get(String hostServer, String nombreArchivo)
 	{
 		byte[] paquete = crearPaqueteRRQoWRQ(nombreArchivo, 1);
 		if(enviarPaquete(hostServer, 69, paquete)==false)
@@ -18,7 +54,7 @@ public class TestCliente extends TFTP{
 		///////////////////////////...
 	}
 	
-	private void put(String hostServer, File archivo)
+	public void put(String hostServer, File archivo)
 	{
 		//Para un archivo de menos de 512B y sin perdida de paquetes
 		
